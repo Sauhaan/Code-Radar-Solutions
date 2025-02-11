a939b1fd12fb2268b922e736a380c0406a814949
Sauhaan <sharmasauhaan1@gmail.com>
2025-02-11 23:54:54 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int temperature;
+    int dayNumber;
 
-    // Read the temperature in Celsius
-    if (scanf("%d", &temperature) != 1) {
+    // Read the day number
+    if (scanf("%d", &dayNumber) != 1) {
         printf("Invalid input. Please enter an integer.\n");
         return 1; // Indicate an error
     }
 
-    // Check if freezing or above freezing
-    if (temperature <= 0) {
-        printf("Freezing\n");
-    } else {
-        printf("Above Freezing\n");
+    // Check the day number and print the day name
+    switch (dayNumber) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:  // Handle invalid input (numbers outside 1-7)
+            printf("Invalid\n");
     }
 
     return 0; // Indicate successful execution
