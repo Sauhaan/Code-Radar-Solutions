df8026c25cc0c7535ffb2ae6370dde1dbf078648
Sauhaan <sharmasauhaan1@gmail.com>
2025-02-11 23:58:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h> // For exit()
-#include <ctype.h>  // For isspace()
 
 int main() {
     int num1, num2;
     char operator;
-
-    // Read input, handling potential whitespace
-    if (scanf("%d %d %c", &num1, &num2, &operator) != 3) {
-        fprintf(stderr, "Invalid input format. Please enter two integers followed by an operator (+, -, *, /).\n");
-        exit(1);
+    
+    // Taking input in the required format
+    if (scanf("%d%c%d", &num1, &operator, &num2) != 3) {
+        printf("Error: Invalid input\n");
+        return 1;
     }
-
-    // Consume any trailing whitespace after the operator
-    while (isspace(getchar())); // or just getchar(); if you only expect a newline.
-
-    int result;
-
-    switch (operator) {
+    
+    // Performing the operation based on the operator
+    switch(operator) {
         case '+':
-            result = num1 + num2;
+            printf("%d\n", num1 + num2);
             break;
         case '-':
-            result = num1 - num2;
+            printf("%d\n", num1 - num2);
             break;
         case '*':
-            result = num1 * num2;
+            printf("%d\n", num1 * num2);
             break;
         case '/':
             if (num2 == 0) {
-                fprintf(stderr, "Error: Division by zero is not allowed.\n");
-                exit(1);
+                printf("Error: Division by zero\n");
             } else {
-                result = num1 / num2;
+                printf("%d\n", num1 / num2);
             }
             break;
         default:
-            fprintf(stderr, "Error: Invalid operator. Please use +, -, *, or /.\n");
-            exit(1);
+            printf("Error: Invalid operator\n");
     }
-
-    printf("%d\n", result);
-
+    
     return 0;
 }
